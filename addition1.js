fc9252ec3e1a53797a2761503aa6b29a03b7e5f7
MayuriJadhav <mayurijadhav1106@gmail.com>
2024-04-05 19:22:23 +0530

    an addition program which takes input by user

--- addition1.js
+++ addition1.js
@@ -1,4 +1,4 @@
-
+const prompt = require('prompt-sync')();
 const num1 = parseInt(prompt('Enter the first number '));
 const num2 = parseInt(prompt('Enter the second number '));
 const sum = num1 + num2;

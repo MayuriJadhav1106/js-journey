7ddff6cf36e5093b6cb73b7c89d8f586fcd9e860
MayuriJadhav <mayurijadhav1106@gmail.com>
2024-04-06 21:37:14 +0530

    added increment and decrement operator

--- arithmetic1.js
+++ arithmetic1.js
@@ -6,3 +6,26 @@ console.log(x);
 let z = 2 ** 5 ;
 console.log(z);
 
+//increment operator
+let a = 2;
+b = a++; 
+
+let p = 5;
+q = ++p; 
+
+console.log(a);
+console.log(p);
+console.log(b);
+console.log(q);
+
+//decrement operator
+let g = 2;
+h = g++; 
+
+let s = 5;
+t = ++s; 
+
+console.log(g);
+console.log(s);
+console.log(h);
+console.log(t);

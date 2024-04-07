84a1d798b5f3965258e7772e07d10467e8c3a804
MayuriJadhav <mayurijadhav1106@gmail.com>
2024-04-07 21:49:23 +0530

    created a file area.js

---
+++ area.js
@@ -0,0 +1,7 @@
+const prompt = require('prompt-sync')();
+const base = prompt('Enter the base of a triangle: ');
+const height = prompt('Enter the height of a triangle: ');
+
+const area = (base * height) / 2;
+
+console.log("The area of the triangle is: ",areaValue);

27f6f9f3a4812a05e882b00f74fdffd457be3e9c
MayuriJadhav <mayurijadhav1106@gmail.com>
2024-04-08 22:40:16 +0530

    converted kilometer into miles

--- conversion.js
+++ conversion.js
@@ -2,7 +2,7 @@ const prompt = require('prompt-sync')();
 const kilometers = prompt("Enter value in kilometers: ")
 
 
-const factor = 2
+const factor =  0.621371
 const miles = kilometers * factor
 
 console.log(`${kilometers} kilometers is equal to ${miles} miles.`);

f90e4cd867d6d3285183589f3a81ec5dd4f59cd4
MayuriJadhav <mayurijadhav1106@gmail.com>
2024-04-08 22:52:56 +0530

    added area of circle

--- area.js
+++ area.js
@@ -1,8 +1,11 @@
 const prompt = require('prompt-sync')();
 const base = prompt('Enter the base : ');
 const height = prompt('Enter the height: ');
+const radius = prompt('Enter the radius: ')
 const areaTriangle = (base * height) / 2;
 const areaRectangle = base * height;
+const areaCircle = 3.14 * (radius **2);
 
 console.log("The area of the triangle is: ",areaTriangle);
 console.log("The area of the rectangle is: ",areaRectangle);
+console.log("The area of the circle is: ",areaCircle);
